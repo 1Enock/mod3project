76dddd24c6ebd7c0ad730e7c585fc3c2c0fa75b3
Enock <enock.kibet@student.moringaschool.com>
2026-05-16 14:56:01 +0300

    Added firebase initialization

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -9,15 +9,15 @@ import {
   onAuthStateChanged,
 } from 'firebase/auth'
 
-// TODO: replace with your Firebase web config
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-}
+  apiKey: "AIzaSyC_vwMajnutmHyyrr2FbUCXDsGvrWvc2As",
+  authDomain: "mod3project-727a0.firebaseapp.com",
+  projectId: "mod3project-727a0",
+  storageBucket: "mod3project-727a0.firebasestorage.app",
+  messagingSenderId: "1072620021873",
+  appId: "1:1072620021873:web:c9538d4ff6a04619c8d3d0",
+  measurementId: "G-G8X6KV0W3W"
+};
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
